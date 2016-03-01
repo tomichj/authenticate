5133d9339bc275b4682128777aa0cc1bf9697a6f
Justin Tomich <tomichj@gmail.com>
2016-02-29 22:06:48 -0800

    added features tests for password resets

--- spec/features/password_reset_spec.rb
+++ spec/features/password_reset_spec.rb
@@ -67,3 +67,57 @@ end
 def expect_mailer_to_have_no_deliveries
   expect(ActionMailer::Base.deliveries).to be_empty
 end
+
+
+
+
+
+feature 'visitor sets new password' do
+  scenario 'requests password change' do
+    user = given_user_with_password_reset_token
+    visit_password_update_page_for user
+    request_password_change_for user
+    expect_password_is_changed_for user
+    expect_redirect_to_root
+  end
+
+  scenario 'attempts password change with fake password reset token' do
+    user = given_user_with_fake_password_reset_token
+    visit_password_update_page_for user
+    expect_failure_flash
+  end
+end
+
+
+def given_user_with_fake_password_reset_token
+  user = create :user
+  user.password_reset_token = 'big_fake_token'
+  user
+end
+
+def given_user_with_password_reset_token
+  create :user, :with_password_reset_token_and_timestamp
+end
+
+def visit_password_update_page_for user
+  visit edit_users_password_path(user.id, token: user.password_reset_token)
+end
+
+def request_password_change_for user
+  fill_in 'password_reset_password', with: 'new_dumb_password'
+  click_button 'Save this password'
+end
+
+def expect_password_is_changed_for user
+  old_encrypted_password = user.encrypted_password
+  expect(user.reload.encrypted_password).to_not eq old_encrypted_password
+end
+
+def expect_redirect_to_root
+  expect(current_path).to eq Authenticate.configuration.redirect_url
+end
+
+def expect_failure_flash
+  expect(page).to have_content 'Please double check the URL or try submitting the form again.'
+end
+
