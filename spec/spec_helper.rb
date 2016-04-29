f899fadc939ca3e561b5510f25b3b484ccbcabe5
Justin Tomich <tomichj@gmail.com>
2016-04-29 10:43:11 -0700

    generating dummy/db in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,13 @@ DatabaseCleaner.strategy = :truncation
 # Load factory girl factories.
 Dir[File.join(File.dirname(__FILE__), 'factories/**/*.rb')].each { |f| require f }
 
+# Build test database in rails
+if defined?(ActiveRecord::Migration.maintain_test_schema!)
+  ActiveRecord::Migration.maintain_test_schema! # rails 4.1+
+else
+  ActiveRecord::Migration.check_pending! # rails 4.0
+end
+
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.infer_spec_type_from_file_location!
